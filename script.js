16472d45594a010a6fb64fd2d4e70902520178f8
yuu-1204 <104064236+uyukigakata@users.noreply.github.com>
2025-03-16 14:22:21 +0900

    push

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    if (targetElement) {
+      window.scrollTo({
+        top: targetElement.offsetTop - 50, // ヘッダー分のオフセット
+        behavior: 'smooth'
+      });
+    }
+  });
+});
